4c8f1f51c9b3e085b7cffa8ba6ff7b8cb2d2eb05
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-16 17:51:44 -0300

    feat: :sparkles: Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -7,13 +9,20 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession();
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn();
             return;
         }
 
-        //TODO Criação da checkout session
+        try {
+            const response = await api.post("/subscribe");
+            const { sessionId } = response.data;
+            const stripe = await getStripeJs();
+            await stripe.redirectToCheckout({sessionId});
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
     return (
