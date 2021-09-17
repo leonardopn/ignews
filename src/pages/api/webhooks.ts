6099e0574f4cae0d7fa3199883b1c6e728c39b6b
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-17 16:49:31 -0300

    feat: :sparkles: Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -38,7 +39,22 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
         const { type } = event;
 
         if (relevantEvents.has(type)) {
-            console.log("Evento recebido", event);
+            try {
+                switch (type) {
+                    case "checkout.session.completed":
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        );
+                        break;
+                    default:
+                        throw new Error("Unhandled event");
+                }
+            } catch (error) {
+                return res.json({ error: "Webhook handle failed." });
+            }
         }
 
         res.json({ received: true });
